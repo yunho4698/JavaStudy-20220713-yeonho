7a99dc4fff3a0a76f5bdc638e94d65326fb03aa9
yunho4698 <dusgh0624@naver.com>
2022-07-24 21:31:34 +0900

    baekjoon_2739

---
+++ src/baekjoon/Baekjoon_2739.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+public class Baekjoon_2739 {
+
+	public static void main(String[] args) {
+		int x= 1;
+		for(int i = 1; i < 10; i++) {
+			System.out.println(x + " * " + i + " = " + x * i);
+		}
+	}
+
+}
